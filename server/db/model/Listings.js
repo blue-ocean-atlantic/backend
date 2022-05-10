e39e20c2e612319096bdfd55598eb3e08c4090df
Vince <vincent.tran.ece@gmail.com>
2022-05-10 18:03:25 -0500

    creates a search route and adds db-loader

--- server/db/model/Listings.js
+++ server/db/model/Listings.js
@@ -2,6 +2,38 @@ const mongoose = require('mongoose');
 
 const tempSchema = new mongoose.Schema({},{strict: false});
 
+const locationSchema = new mongoose.Schema({
+  address: {
+    type: String
+  },
+  street: {
+    type: String
+  },
+  state: {
+    type: String,
+    minLength: 2,
+    maxLength: 2
+  },
+  street: {
+    type: String
+  },
+  zipcode: {
+    type: String,
+    minLength: 5,
+    maxLength: 5,
+    required: true
+    // type: Number,
+    // min: 501,
+    // max: 99950,
+  },
+  longitute: {
+    type: Number
+  },
+  latitude: {
+    type: Number
+  }
+});
+
 const listingSchema = new mongoose.Schema({
   listing_id: {
     type: Number,
@@ -42,8 +74,8 @@ const listingSchema = new mongoose.Schema({
     // max: 99950,
   },
   location: {
-    type: locationSchema
-  }
+    type: tempSchema,
+  },
   ended: {
     type: Boolean,
     required: true,
@@ -53,38 +85,9 @@ const listingSchema = new mongoose.Schema({
     type: Date
   }
 });
+
 listingSchema.index({ listing_id: 1 });
 
-const locationSchema = new mongoose.Schema({
-  address: {
-    type: String
-  },
-  street: {
-    type: String
-  },
-  state: {
-    type: String,
-    minLength: 2,
-    maxLength: 2
-  },
-  street: {
-    type: String
-  },
-  zipcode: {
-    type: String,
-    minLength: 5,
-    maxLength: 5,
-    required: true
-    // type: Number,
-    // min: 501,
-    // max: 99950,
-  },
-  longitute: {
-    type: Number
-  },
-  latitude: {
-    type: Number
-  }
-});
+
 
 module.exports = mongoose.model('listings', tempSchema);
