708ac9631ffe99ad48a1bac959a08060e6d9f18d
Vince <vincent.tran.ece@gmail.com>
2022-05-11 14:54:30 -0500

    seperates routes from server index

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
+require("./db");
+require("dotenv").config();
+const router = require('./routes');
+
 /* === External Modules === */
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-// const bp = require('body-parser');
-const { searchListings, details, createPost, getLanding } = require("./db/controllers");
-require("./db");
-
-require("dotenv").config();
-
-/* === ImageKit authentication === */
-const ImageKit = require("imagekit");
-// const fs = require('fs');
 
 /* === Server Configuration === */
 const PORT = process.env.PORT || 3000;
@@ -20,94 +15,12 @@ const app = express();
 
 /* === Middleware === */
 app.use(express.json({ limit: 500 }));
-// app.use(bp.json());
+app.use('/', router); // this sends stuff to the router
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // serve static files
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-/* === API Routes === */
-
-app.get("/api/imagekit", (req, res) => {
-  // Look into if this needs to have additional measures for security.
-  // i.e. send only if source of request is our website?
-
-  const imagekit = new ImageKit({
-    publicKey: "public_FMjtxsWyzDWFsDCkU+3LPha1J2E=",
-    privateKey: process.env.IMGKIT_PRIVATE_KEY,
-    urlEndpoint: "https://ik.imagekit.io/joshandromidas/",
-  });
-
-  const authenticationParameters = imagekit.getAuthenticationParameters();
-
-  res.json(authenticationParameters);
-});
-
-/* === Page Routes === */
-
-//product/map page
-app.get("/results", (req, res) => {
-  const {
-    query: { zipCode },
-  } = req;
-
-  searchListings(zipCode)
-    .then((response) => {
-      res.send(response);
-    })
-    .catch((error) => {
-      console.log("ERR product/map page route", err);
-      res.send(error);
-    });
-});
-
-//product/service info page
-app.get("/details", (req, res) => {
-  const id = req.query.id;
-  details(id)
-    .then((response) => {
-      res.send(response);
-    })
-    .catch((err) => {
-      console.log("ERR product/service route", err);
-      res.send(err);
-    });
-});
-
-//create a post page
-app.post("/las", (req, res) => {
-  const {
-    user_id,
-    title,
-    description,
-    type,
-    images,
-    available_date } = req.body;
-
-  createPost(user_id, title, description, type, images, available_date)
-  .then((results) => {
-    console.log(results);
-    req.send(results)
-  })
-  .catch((err) => {
-    console.log('something went wrong in createPost');
-    res.send(err);
-  })
-
-});
-
-// getLanding
-app.get("/api/listings/landing", (req, res) => {
-  getLanding()
-  .then((results) => {
-    res.send(results)
-  })
-  .catch((err) => {
-    console.log('something broke while getting landing', err);
-    res.send(err);
-  })
-});
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
 /* === Server Listener === */
 app.listen(PORT, () => {
