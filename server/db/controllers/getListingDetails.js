0775d2b6380717982d355d91a447a2259da9ce91
Vince <vincent.tran.ece@gmail.com>
2022-05-12 17:38:49 -0500

    resolves merge conflicts

--- server/db/controllers/getListingDetails.js
+++ server/db/controllers/getListingDetails.js
@@ -2,20 +2,6 @@ const { Listings } = require('../model');
 
 // 5) getListingDetails
 module.exports = async function (id) {
-<<<<<<< HEAD
-
-    console.log('inside get listing details')
-  try {
-    let listingData = await Listings.findOne({listing_id: id}).select('-_id').limit(10)
-    .populate('donor', '-_id -password -__v');
-
-    return listingData;
-  } catch(err) {
-    console.log('something went wrong inside getListingDetails: ', err.message);
-    return err.message;
-  }
-
-=======
     try {
       let results = await Listings.find({}).select('-_id').limit(10);
       return results;
@@ -23,5 +9,6 @@ module.exports = async function (id) {
       console.log('something went wrong inside searchListings: ', err.message);
       return err.message;
     }
->>>>>>> main
 }
+
+
--- server/db/controllers/getUserInfo.js
+++ server/db/controllers/getUserInfo.js
@@ -3,20 +3,12 @@ const { Users } = require('../model');
 module.exports = async function (id) {
   console.log('this is it', id)
   // const query = zipcode ? {zipcode: zipcode} : {};
-<<<<<<< HEAD
-  try {
-    let userProfile = await Users.findOne({user_id: id}).select('-_id -password -__v -zipcode -bio -email -id')
-    // .populate('ratingDetails');
-    return userProfile;
-=======
-
 
   if(!id) {
     try {
       let results = await Users.findOne({}).sort({user_id: -1}).select('-_id');
           return results;
-      
->>>>>>> main
+
   } catch(err) {
     console.log('something went wrong inside getUserDetails: ', err.message);
     return err.message;
