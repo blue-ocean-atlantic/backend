aff29ac59573f2c286adb53ace113de15278b20f
Vince <vincent.tran.ece@gmail.com>
2022-05-12 14:05:03 -0500

    completes routes 1-5

--- server/db/controllers/getUserInfo.js
+++ server/db/controllers/getUserInfo.js
@@ -4,7 +4,7 @@ module.exports = async function (id) {
   console.log('this is it', id)
   // const query = zipcode ? {zipcode: zipcode} : {};
   try {
-    let userProfile = await Users.find({id}).select('-_id').limit(10);
+    let userProfile = await Users.findOne({user_id: id}).select('-_id -password -__v -zipcode -bio -email -id').populate('ratingDetails');
     return userProfile;
   } catch(err) {
     console.log('something went wrong inside searchListings: ', err.message);
