9680d527fb8ddf2dac068c02a6c75a583de26422
Ricky Polim <ricpolim@gmail.com>
2022-05-12 17:53:15 -0700

    modified db-loader so that the loaded data will create "createdAt" upon creation

--- db-loader/index.js
+++ db-loader/index.js
@@ -3,8 +3,19 @@ const listingsData = require('./listings_mock_data.json');
 // const chatMessages = require('./chat_messages_mock.json');
 const ratingsData = require('./rating_mock_data.json');
 const usersData = require('./users_mock_data.json');
-const appointments = require('./appointments.json');
-const zipCodes = require('./zipcodes.json');
+const appointmentsData = require('./appointments.json');
+const zipcodesData = require('./zipcodes.json');
+
+const {
+  Users,
+  Listings,
+  Ratings,
+  Appointments,
+  Chats,
+  ChatMessages,
+  Zipcodes
+} = require('../server/db/model');
+
 const mongoose = require('mongoose');
 
 var url = 'mongodb://localhost:27017/blue_ocean_db';
@@ -15,17 +26,15 @@ mongoose.connect(url, {autoIndex: false}, () => {
 
 }, e => console.log('error: ',e))
 
+// let schema = new mongoose.Schema({},{strict: false});
 
-let schema = new mongoose.Schema({},{strict: false});
-
-const Listings = mongoose.model('listings', schema);
-const Chatroom = mongoose.model('chats', schema);
-const Messages = mongoose.model('chats_messages', schema);
-const Ratings = mongoose.model('ratings', schema);
-const Users = mongoose.model('users', schema);
-const Appointments = mongoose.model('appointments', schema);
-const ZipCodes = mongoose.model('zipcodes', schema);
-
+// const Listings = mongoose.model('listings', schema);
+// const Chatroom = mongoose.model('chats', schema);
+// const Messages = mongoose.model('chats_messages', schema);
+// const Ratings = mongoose.model('ratings', schema);
+// const Users = mongoose.model('users', schema);
+// const Appointments = mongoose.model('appointments', schema);
+// const ZipCodes = mongoose.model('zipcodes', schema);
 
 async function addDummyData () {
   try {
@@ -35,15 +44,15 @@ async function addDummyData () {
     await Ratings.remove({});
     await Users.remove({});
     await Appointments.remove({});
-    await ZipCodes.remove({});
+    await Zipcodes.remove({});
 
     await Listings.insertMany(listingsData);
     // await Chatroom.insertMany(chatroomData);
     // await Messages.insertMany(chatMessages);
     await Ratings.insertMany(ratingsData);
     await Users.insertMany(usersData);
-    await Appointments.insertMany(usersData);
-    await ZipCodes.insertMany(zipCodes);
+    await Appointments.insertMany(appointmentsData);
+    await Zipcodes.insertMany(zipcodesData);
     console.log('data loading complete!');
     process.exit();
   } catch (err) {
